dde667d5a6b0083caedc39f8938e67f09af04ea7
warren <zhouhualei@gmail.com>
2015-04-19 23:17:10 +0800

    Problem 146: add LRUCache implemented by Guava Cache

---
+++ src/main/java/me/warren/leetcode/LRUCacheByGuava.java
@@ -0,0 +1,33 @@
+package me.warren.leetcode;
+
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+
+/**
+ * Created by warzhou1 on 4/19/15.
+ * Problem 146
+ * Make use of Guava CacheBuilder to implement a LRUCache
+ */
+public class LRUCacheByGuava {
+
+    private Cache<Integer, Integer> cache;
+
+    public LRUCacheByGuava(int capacity) {
+        cache = CacheBuilder.newBuilder().maximumSize(capacity).build();
+    }
+
+    public int get(int key) throws Exception {
+        Object value = cache.getIfPresent(key);
+        if (value == null) {
+            return -1;
+        }
+        return (Integer) value;
+    }
+
+    public void set(int key, int value) {
+        cache.put(key, value);
+    }
+
+}
+
--- src/test/java/me/warren/leetcode/LRUCacheByGuavaSpec.groovy
+++ src/test/java/me/warren/leetcode/LRUCacheByGuavaSpec.groovy
@@ -5,10 +5,10 @@ import spock.lang.Specification
 /**
  * Created by warzhou1 on 4/19/15.
  */
-class LRUCacheByLRUMapSpec extends Specification {
+class LRUCacheByGuavaSpec extends Specification {
     def "test Cache Simple Get and Set"() {
         setup: "a cache instace is created"
-            def cache = new LRUCacheByLRUMap(10)
+            def cache = new LRUCacheByGuava(10)
         when:
             cache.set(2, 1)
         then:
@@ -22,7 +22,7 @@ class LRUCacheByLRUMapSpec extends Specification {
 
     void "test Cache Removal"() {
         setup: "a cache instace is created"
-            def cache = new LRUCacheByLRUMap(1);
+            def cache = new LRUCacheByGuava(1);
         when:
             cache.set(2, 1);
         then:
